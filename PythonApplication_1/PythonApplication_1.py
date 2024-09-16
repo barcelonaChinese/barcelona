ed1710a4813af4a9ac8094191ecdf7c56b16a36a
barcelonaChinese <13716505821@163.com>
2024-09-17 00:34:56 +0800

    add

--- PythonApplication_1/PythonApplication_1.py
+++ PythonApplication_1/PythonApplication_1.py
@@ -1,1 +1,32 @@
-print (111)
+﻿print("+---------------------------------------------------------------------+")
+print("|                                                                     |")
+print("|                   花有重开日, 人无再少年                               |")
+print("|                                                                     |")
+print("|                   欢迎来到, 人生重开模拟器                             |")
+print("|                                                                     |")
+print("+---------------------------------------------------------------------+")
+# 设置初始属性
+while True:
+    print("请设定初始属性(可用总点数 20)")
+    face = int(input("设定 颜值(1-10):"))
+    strong = int(input("设定 体质(1-10):"))
+    iq = int(input("设定 智力(1-10):"))
+    home = int(input("设定 家境(1-10):"))
+
+    if face < 1 or face > 10:
+        print("颜值设置有误!")
+        continue
+    if strong < 1 or strong > 10:
+        print("体质设置有误!")
+        continue
+    if iq < 1 or iq > 10:
+        print("智力设置有误!")
+        continue
+    if home < 1 or home > 10:
+        print("家境设置有误!")
+        continue
+    if face + strong + iq + home > 20:
+        print("总点数超过了 20!")
+        continue
+    print("初始属性设定完成!")
+    break
--- pythonProject_2/test.py
+++ pythonProject_2/test.py
@@ -1,3 +1,41 @@
-from sklearn import datasets
-iris = datasets.load_iris()
-print(iris.DESCR)
+import math
+
+# while True:
+#     face =int(input("face:"))
+#
+#     if face<1 or face>10:
+#         print("wrong")
+#         continue
+#     print("finish")
+#     break
+
+def test():
+    print("test")
+
+a=(1,2,3)
+b=[4,5,6]
+c={7:"a",8:"b",9:"c"}
+print(4 in a)
+print(4 in b)
+print(7 in c)
+print("c" in c)
+
+# for a in range(1,3):
+#     print(a)
+#
+# for b in range(1,3):
+#     print(b)
+
+for k in a:
+    print(k)
+
+for k in b:
+    print(k)
+
+for k in c:
+    print(k,":",c[k])
+
+test()
+
+print(math.exp(2))
+
